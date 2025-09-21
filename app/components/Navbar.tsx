811afb07e25aa72ee1718ba4b3aa2cf21a9b8b65
Hajo <ahmedhaj000@gmail.com>
2025-09-21 11:40:46 +0300

    added shadow to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,9 +2,9 @@ import {Link} from "react-router";
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <nav className="navbar shadow-2xl">
             <Link to="/">
-                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+                <p className="text-2xl font-bold text-gradient">RESUMATE</p>
             </Link>
             <Link to="/upload" className="primary-button w-fit">
                 Upload Resume
